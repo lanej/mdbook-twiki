2e3f09efb11c0c6a543ed315301adc6296d1b949
Joshua Lane <me@joshualane.com>
2022-07-17 21:48:26 -0700

    ref: anchor support and twiki config

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,46 @@ extern crate mdbook;
 
 use std::fs::{self, File};
 use std::io;
-use twiki::to_twiki;
+use twiki::Twiki;
 
 use mdbook::{renderer::RenderContext, BookItem};
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(default, rename_all = "kebab-case")]
-pub struct WordcountConfig {
+pub struct Config {
     pub ignores: Vec<String>,
     pub filename: String,
+    pub page_url: Option<String>,
 }
 
-// let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            filename: "index.twiki".to_string(),
+            ignores: vec![],
+            page_url: None,
+        }
+    }
+}
 
 fn main() {
     let mut stdin = io::stdin();
     let ctx = RenderContext::from_json(&mut stdin).unwrap();
     let _ = fs::create_dir_all(&ctx.destination);
-    let mut f = File::create(ctx.destination.join("index.twiki")).unwrap();
 
-    let cfg: WordcountConfig = ctx
+    let cfg: Config = ctx
         .config
-        .get_deserialized_opt("output.wordcount")
+        .get_deserialized_opt("output.twiki")
         .unwrap_or_default()
         .unwrap_or_default();
+
+    let mut f = File::create(ctx.destination.join("index.twiki")).unwrap();
+    let twiki = Twiki {
+        base_url: cfg.page_url,
+    };
 
     // TODO: generate the TOC first
     // TODO: are chapters iterated in order for the SUMMARY.md
@@ -40,7 +53,7 @@ fn main() {
                 continue;
             }
 
-            to_twiki(&ch.content, &mut f).unwrap();
+            twiki.convert(&ch.content, &mut f).unwrap();
         }
     }
 }
--- src/twiki.rs
+++ src/twiki.rs
@@ -1,112 +1,165 @@
-use pulldown_cmark::{CowStr, Event, Options, Parser, Tag};
+use pulldown_cmark::{CowStr, Event, LinkType, Options, Parser, Tag};
 
-// https://twiki.org/cgi-bin/view/TWiki05x01/TextF&ormattingRules
-// TODO: separate markdown parsing and twiki formation
-pub fn to_twiki(content: &str, output: &mut dyn std::io::Write) -> std::io::Result<()> {
-    let mut options = Options::empty();
-    let mut code_block = false;
-    let mut in_list = false;
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_TABLES);
-    options.insert(Options::ENABLE_FOOTNOTES);
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_TASKLISTS);
-    // options.insert(Options::ENABLE_SMART_PUNCTUATION);
-    // options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+#[derive(Default)]
+pub struct Twiki {
+    pub base_url: Option<String>,
+}
 
-    for e in Parser::new_ext(&content, options) {
-        match e {
-            Event::Start(s) => match s {
-                Tag::Paragraph => {}
-                Tag::Heading(level, id, _) => {
-                    let mut twiki_level = String::new();
-                    for _ in 0..(level as u8) {
-                        twiki_level.push('+');
+impl Twiki {
+    // https://twiki.org/cgi-bin/view/TWiki05x01/TextF&ormattingRules
+    // TODO: separate markdown parsing and twiki formation
+    pub fn convert(&self, content: &str, output: &mut dyn std::io::Write) -> std::io::Result<()> {
+        let mut options = Options::empty();
+        let mut code_block = false;
+        let mut in_list = false;
+        let mut emphasis = false;
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        options.insert(Options::ENABLE_TABLES);
+        options.insert(Options::ENABLE_FOOTNOTES);
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        options.insert(Options::ENABLE_TASKLISTS);
+        // options.insert(Options::ENABLE_SMART_PUNCTUATION);
+        // options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+
+        for e in Parser::new_ext(&content, options) {
+            match e {
+                Event::Start(s) => match s {
+                    Tag::Paragraph => {}
+                    Tag::Heading(level, id, _) => {
+                        let mut twiki_level = String::new();
+                        for _ in 0..(level as u8) {
+                            twiki_level.push('+');
+                        }
+                        write!(output, "---{} {}", twiki_level, id.unwrap_or_default())?
                     }
-                    write!(output, "---{} {}", twiki_level, id.unwrap_or_default())?
+                    Tag::BlockQuote => writeln!(output, "<verbatim>")?,
+                    Tag::CodeBlock(_) => {
+                        code_block = true;
+                        writeln!(output, "<verbatim>")?
+                    }
+                    Tag::List(_) => {
+                        in_list = true;
+                    }
+                    Tag::Item => {}
+                    Tag::FootnoteDefinition(_) => todo!(),
+                    Tag::Table(_) => todo!(),
+                    Tag::TableHead => todo!(),
+                    Tag::TableRow => todo!(),
+                    Tag::TableCell => todo!(),
+                    Tag::Strong => {
+                        if emphasis {
+                            write!(output, "_")?
+                        } else {
+                            write!(output, "*")?
+                        }
+                    }
+                    Tag::Emphasis => {
+                        emphasis = true;
+                        write!(output, "_")?
+                    }
+                    Tag::Strikethrough => write!(output, "<strike>")?,
+                    Tag::Link(t, url, _title) => match t {
+                        LinkType::Inline => write!(output, "[[{}][", self.anchor_target(url),)?,
+                        LinkType::Reference => todo!(),
+                        LinkType::ReferenceUnknown => todo!(),
+                        LinkType::Collapsed => todo!(),
+                        LinkType::CollapsedUnknown => todo!(),
+                        LinkType::Shortcut => todo!(),
+                        LinkType::ShortcutUnknown => todo!(),
+                        LinkType::Autolink => todo!(),
+                        LinkType::Email => write!(output, "<a href=\"mailto:{}\">", url)?,
+                    },
+                    Tag::Image(_, _, _) => todo!(),
+                },
+                Event::End(ee) => match ee {
+                    Tag::Paragraph => write!(output, "\n")?,
+                    Tag::Heading(_, _, _) => write!(output, "\n")?,
+                    Tag::BlockQuote => write!(output, "</verbatim>\n")?,
+                    Tag::CodeBlock(_) => {
+                        code_block = false;
+                        write!(output, "</verbatim>\n")?
+                    }
+                    Tag::List(_) => {
+                        in_list = false;
+                    }
+                    Tag::Item => write!(output, "\n")?,
+                    Tag::FootnoteDefinition(_) => todo!(),
+                    Tag::Table(_) => todo!(),
+                    Tag::TableHead => todo!(),
+                    Tag::TableRow => todo!(),
+                    Tag::TableCell => todo!(),
+                    Tag::Strong => {
+                        if emphasis {
+                            write!(output, "_")?
+                        } else {
+                            write!(output, "*")?
+                        }
+                    }
+                    Tag::Emphasis => {
+                        emphasis = false;
+                        write!(output, "_")?
+                    }
+                    Tag::Strikethrough => write!(output, "</strike>")?,
+                    Tag::Link(t, _url, _title) => match t {
+                        LinkType::Inline => write!(output, "]]")?,
+                        LinkType::Reference => todo!(),
+                        LinkType::ReferenceUnknown => todo!(),
+                        LinkType::Collapsed => todo!(),
+                        LinkType::CollapsedUnknown => todo!(),
+                        LinkType::Shortcut => todo!(),
+                        LinkType::ShortcutUnknown => todo!(),
+                        LinkType::Autolink => todo!(),
+                        LinkType::Email => write!(output, "</a>")?,
+                    },
+                    Tag::Image(_, _, _) => todo!(),
+                },
+                Event::Text(s) => {
+                    if code_block && s.trim_start().starts_with("```") {
+                        // pass
+                    } else if code_block {
+                        write!(output, "{}", s.trim_end().strip_suffix("```").unwrap_or(&s))
+                            .unwrap();
+                    } else if in_list {
+                        let item = s.to_string();
+                        let mut prefix = String::new();
+                        item.chars()
+                            .take_while(|c| '*' == *c)
+                            .skip(1)
+                            .for_each(|_| prefix.push_str("\t"));
+                        write!(
+                            output,
+                            "{}* {}",
+                            prefix,
+                            item.trim_start_matches("*").trim_start()
+                        )?;
+                    } else {
+                        write!(output, "{}", s)?;
+                    }
                 }
-                Tag::BlockQuote => writeln!(output, "<verbatim>")?,
-                Tag::CodeBlock(_) => {
-                    code_block = true;
-                    writeln!(output, "<verbatim>")?
+                Event::Code(c) => match c {
+                    CowStr::Boxed(s) => write!(output, "={}=", s)?,
+                    CowStr::Inlined(s) => write!(output, "={}=", s)?,
+                    CowStr::Borrowed(s) => write!(output, "={}=", s)?,
+                },
+                Event::Html(tag) => write!(output, "{}", tag)?,
+                Event::FootnoteReference(_) => todo!(),
+                Event::SoftBreak | Event::HardBreak | Event::TaskListMarker(_) => {
+                    write!(output, "\n")?
                 }
-                Tag::List(_) => {
-                    in_list = true;
-                }
-                Tag::Item => {}
-                Tag::FootnoteDefinition(_) => todo!(),
-                Tag::Table(_) => todo!(),
-                Tag::TableHead => todo!(),
-                Tag::TableRow => todo!(),
-                Tag::TableCell => todo!(),
-                Tag::Emphasis => write!(output, "*")?,
-                Tag::Strong => {}
-                Tag::Strikethrough => write!(output, "<strike>")?,
-                Tag::Link(_, _, _) => todo!(),
-                Tag::Image(_, _, _) => todo!(),
-            },
-            Event::End(ee) => match ee {
-                Tag::Paragraph => write!(output, "\n")?,
-                Tag::Heading(_, _, _) => write!(output, "\n")?,
-                Tag::BlockQuote => write!(output, "</verbatim>\n")?,
-                Tag::CodeBlock(_) => {
-                    code_block = false;
-                    write!(output, "</verbatim>\n")?
-                }
-                Tag::List(_) => {
-                    in_list = false;
-                }
-                Tag::Item => write!(output, "\n")?,
-                Tag::FootnoteDefinition(_) => todo!(),
-                Tag::Table(_) => todo!(),
-                Tag::TableHead => todo!(),
-                Tag::TableRow => todo!(),
-                Tag::TableCell => todo!(),
-                Tag::Emphasis => write!(output, "*")?,
-                Tag::Strong => {
-                    write!(output, "*").unwrap();
-                }
-                Tag::Strikethrough => write!(output, "</strike>")?,
-                Tag::Link(_, _, _) => todo!(),
-                Tag::Image(_, _, _) => todo!(),
-            },
-            Event::Text(s) => {
-                if code_block && s.trim_start().starts_with("```") {
-                    // pass
-                } else if code_block {
-                    write!(output, "{}", s.trim_end().strip_suffix("```").unwrap_or(&s)).unwrap();
-                } else if in_list {
-                    let item = s.to_string();
-                    let mut prefix = String::new();
-                    item.chars()
-                        .take_while(|c| '*' == *c)
-                        .skip(1)
-                        .for_each(|_| prefix.push_str("\t"));
-                    write!(
-                        output,
-                        "{}* {}",
-                        prefix,
-                        item.trim_start_matches("*").trim_start()
-                    )?;
-                } else {
-                    write!(output, "{}", s)?;
-                }
+                Event::Rule => write!(output, "\n---\n")?,
             }
-            Event::Code(c) => match c {
-                CowStr::Boxed(s) => write!(output, "={}=", s)?,
-                CowStr::Inlined(s) => write!(output, "={}=", s)?,
-                CowStr::Borrowed(s) => write!(output, "={}=", s)?,
-            },
-            Event::Html(tag) => write!(output, "{}", tag)?,
-            Event::FootnoteReference(_) => todo!(),
-            Event::SoftBreak => write!(output, "\n")?,
-            Event::HardBreak => write!(output, "\n")?,
-            Event::Rule => write!(output, "\n---\n")?,
-            Event::TaskListMarker(_) => todo!(),
         }
+
+        Ok(())
     }
 
-    Ok(())
+    fn anchor_target(&self, url: CowStr) -> String {
+        if url.starts_with("https://") || url.starts_with("http://") {
+            url.to_string()
+        } else {
+            url.chars().skip_while(|c| '#' != *c).collect::<String>()
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -149,7 +202,7 @@ impl std::io::Write for StringIO {
 #[allow(dead_code)]
 fn markdown_to_twiki_string(markdown: String) -> String {
     let mut output = StringIO::new();
-    to_twiki(&markdown, &mut output).unwrap();
+    Twiki::default().convert(&markdown, &mut output).unwrap();
     return output.to_string();
 }
 
@@ -176,10 +229,20 @@ fn test_conversion() {
             paragraph 2"#,
             "paragraph 1\nparagraph 2\n",
         ],
-        ["*bold*", "*bold*\n"],
+        ["_italic_", "_italic_\n"],
+        ["**bold**", "*bold*\n"],
+        ["***bolditalic***", "__bolditalic__\n"],
         [
             "* item 1\n** item 2\n*** item 3\n",
             "* item 1\n\t* item 2\n\t\t* item 3\n",
+        ],
+        [
+            "[title](./file.md#heading-title)",
+            "[[#heading-title][title]]\n",
+        ],
+        [
+            "[title](https://www.example.org)",
+            "[[https://www.example.org][title]]\n",
         ],
     ];
     matrix.iter().for_each(|[input, expected]: &[&str; 2]| {
